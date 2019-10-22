2256e2bc97bd9b685c489ab3f5d6e85de2015e71
Filip Fafara <ffafara@thoughtworks.com>
2019-10-22 15:44:20 -0400

    Implement user and order routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import * as bodyParser from 'body-parser'
 import * as express from 'express'
-import { Index } from './routes/index'
+import { APIRoute } from './routes/api'
+import { OrderRoute } from './routes/order'
+import { UserRoute } from './routes/user'
 
 class App {
   public app: express.Application
-  public indexRoutes: Index = new Index()
+  public apiRoutes: APIRoute = new APIRoute()
+  public userRoutes: UserRoute = new UserRoute()
+  public orderRoutes: OrderRoute = new OrderRoute()
 
   constructor() {
     this.app = express()
     this.app.use(bodyParser.json())
-    this.indexRoutes.routes(this.app)
+    this.apiRoutes.routes(this.app)
+    this.userRoutes.routes(this.app)
+    this.orderRoutes.routes(this.app)
   }
 }
 
---
+++ src/controllers/order.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from 'express'
+import * as _ from 'lodash'
+import Order from '../models/order'
+import { OrderStatus } from '../models/orderStatus'
+
+let orders: Array<Order> = []
+
+export let getOrder = (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id
+  const order = orders.find(obj => obj.id === Number(id))
+  const httpStatusCode = order ? 200 : 404
+  return res.status(httpStatusCode).send(order)
+}
+
+export let addOrder = (req: Request, res: Response, next: NextFunction) => {
+  /* tslint:disable:object-literal-sort-keys */
+  const order: Order = {
+    // generic random value from 1 to 100 only for tests so far
+    id: Math.floor(Math.random() * 100) + 1,
+    userId: req.body.userId,
+    quantity: req.body.quantity,
+    shipDate: req.body.shipDate,
+    status: OrderStatus.Placed,
+    complete: false,
+  }
+  /* tslint:enable:object-literal-sort-keys */
+  orders.push(order)
+  return res.status(201).send(order)
+}
+
+export let removeOrder = (req: Request, res: Response, next: NextFunction) => {
+  const id = Number(req.params.id)
+  const orderIndex = orders.findIndex(item => item.id === id)
+
+  if (orderIndex === -1) {
+    return res.status(404).send()
+  }
+
+  orders = orders.filter(item => item.id !== id)
+
+  return res.status(204).send()
+}
+
+export let getInventory = (req: Request, res: Response, next: NextFunction) => {
+  const grouppedOrders = _.groupBy(orders, 'userId')
+  return res.status(200).send(grouppedOrders)
+}
